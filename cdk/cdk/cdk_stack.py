1ae49fa8ac2ab96dab60a264caeca1e04cce0dcb
Juraj Paluba <juraj.palubajp@gmail.com>
2025-05-16 20:34:46 +0200

    feat(stack): define root VPC
    As simple as possible to reduce costs. I need internet access as I will
    be downloading some apt packages.
    
    ---v2
    I don't need private subnet with NAT. Just plain public subnet will do.
    The DB instance will be accessible from the internet.

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,12 +1,20 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
 )
 from constructs import Construct
+
+from aws_cdk.aws_ec2 import Vpc, IpAddresses, SubnetConfiguration, SubnetType
 
 
 class CdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        vpc = Vpc(
+            self, "PostgresVPC", ip_addresses=IpAddresses.cidr("10.0.0.0/25"), availability_zones=["eu-central-1b"],
+            restrict_default_security_group=False, enable_dns_hostnames=False, nat_gateways=0,
+            subnet_configuration=[
+                SubnetConfiguration(name="public", subnet_type=SubnetType.PUBLIC, map_public_ip_on_launch=True),
+            ]
+        )
