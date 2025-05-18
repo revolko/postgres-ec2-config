4759f7e09c78de4aad40a0d9cf5c3c6f53503fdd
Juraj Paluba <juraj.palubajp@gmail.com>
2025-05-18 10:53:20 +0200

    feat(volume): create data volume

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     Stack,
+    Size,
 )
 from constructs import Construct
 
@@ -13,24 +14,32 @@ from aws_cdk.aws_ec2 import (
     InstanceClass,
     InstanceSize,
     MachineImage,
+    Volume,
 )
 
 
 class CdkStack(Stack):
 
+    AVAIL_ZONE = "eu-central-1b"
+
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         vpc = Vpc(
-            self, "PostgresVPC", ip_addresses=IpAddresses.cidr("10.0.0.0/25"), availability_zones=["eu-central-1b"],
+            self, "PostgresVPC", ip_addresses=IpAddresses.cidr("10.0.0.0/25"), availability_zones=[self.AVAIL_ZONE],
             restrict_default_security_group=False, enable_dns_hostnames=False, nat_gateways=0,
             subnet_configuration=[
                 SubnetConfiguration(name="public", subnet_type=SubnetType.PUBLIC, map_public_ip_on_launch=True),
             ]
         )
 
-        Instance(
+        postgres_server = Instance(
             self, "PostgresInstance", vpc=vpc,
             instance_type=InstanceType.of(instance_class=InstanceClass.T3, instance_size=InstanceSize.SMALL),
             machine_image=MachineImage.generic_linux({"eu-central-1": "ami-03250b0e01c28d196"})
         )
+
+        data_volume = Volume(
+            self, "DataVolume", availability_zone=self.AVAIL_ZONE, size=Size.gibibytes(30),
+        )
+        data_volume.grant_attach_volume(postgres_server.role)
